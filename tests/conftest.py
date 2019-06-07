92fa25f7c5ae01faff2baf4119c83a8a922cd312
Mohammad Almogbel <almogbelmohi@gmail.com>
2019-06-08 01:58:39 +0900

    Fix testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -27,7 +27,7 @@ def docker_client():
 @pytest.fixture(scope='session')
 def image_name():
     """Image name to test"""
-    return '@mohirio/mnelab'
+    return 'mohirio/mnelab'
 
 
 class TrackedContainer(object):
